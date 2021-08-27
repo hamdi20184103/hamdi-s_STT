ec6925d07e32d11829a6a0bc95ab4909813ae44d
hamdi20184103 <85195320+hamdi20184103@users.noreply.github.com>
2021-08-28 01:23:18 +0200

    Add files via upload

---
+++ bonus2.js
@@ -0,0 +1,9 @@
+const Players = [
+{name: "Greenwood" , age:19 , team : "Manchester United"},
+{name: "Lukaku" , age:28 , team : "Chelsea"},
+{name: "Bruno" , age:26 , team : "Manchester United"},
+{name: "John" , age:27 , team : "Manchester City"},
+{name: "Kane" , age:28 , team : "TottenHam Spur"},
+]
+
+console.log(Players.find(item => item.name = "John"));
